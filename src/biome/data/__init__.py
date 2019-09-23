6194b83c4e7e80e041fd91dff53534a29f91af3b
David Fidalgo <david@recogn.ai>
2019-09-23 11:54:45 +0200

    add version information

--- src/biome/data/__init__.py
+++ src/biome/data/__init__.py
@@ -1,3 +1,11 @@
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__.replace(".", "-")).version
+except pkg_resources.DistributionNotFound:
+    # package is not installed
+    pass
+
 ENV_DASK_CLUSTER = "DASK_CLUSTER"
 ENV_DASK_CACHE_SIZE = "DASK_CACHE_SIZE"
 DEFAULT_DASK_CACHE_SIZE = 2e9

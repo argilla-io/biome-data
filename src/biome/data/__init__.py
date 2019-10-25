01bb0ba520b723eeb17295aed717426deaa43b23
Francisco Aranda <francisco@recogn.ai>
2019-10-25 12:47:46 +0200

    chore: allow import DataSource from biome.data

--- src/biome/data/__init__.py
+++ src/biome/data/__init__.py
@@ -1,13 +1,13 @@
 import pkg_resources
 
+from .sources import DataSource
+
 try:
     __version__ = pkg_resources.get_distribution(__name__.replace(".", "-")).version
 except pkg_resources.DistributionNotFound:
     # package is not installed
     pass
 
-ENV_DASK_CLUSTER = "DASK_CLUSTER"
+# TODO : remove from here and/or remove if not needed
 ENV_DASK_CACHE_SIZE = "DASK_CACHE_SIZE"
 DEFAULT_DASK_CACHE_SIZE = 2e9
-
-ENV_ES_HOSTS = "ES_HOSTS"

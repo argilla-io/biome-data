b41a0ffa32d78a8f9ba8433807fd6910affab446
Francisco Aranda <francisco@recogn.ai>
2019-10-25 12:48:26 +0200

    chore: disable some unused pylint rules

--- src/biome/data/__init__.py
+++ src/biome/data/__init__.py
@@ -9,5 +9,8 @@ except pkg_resources.DistributionNotFound:
     pass
 
 # TODO : remove from here and/or remove if not needed
+ENV_DASK_CLUSTER = "DASK_CLUSTER"
 ENV_DASK_CACHE_SIZE = "DASK_CACHE_SIZE"
 DEFAULT_DASK_CACHE_SIZE = 2e9
+
+ENV_ES_HOSTS = "ES_HOSTS"

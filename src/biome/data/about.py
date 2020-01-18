f80b963985d53ce0b1788630a600bd7b96d3e1f3
Francisco Aranda <francisco@recogn.ai>
2020-01-18 23:20:51 +0100

    add commit has as version segment

--- src/biome/data/about.py
+++ src/biome/data/about.py
@@ -1,2 +1,22 @@
 # See https://www.python.org/dev/peps/pep-0440/ for version numbering
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.0.dev"
+
+
+def get_commit_hash() -> str:
+    """
+    Fetch current commit hash from configured git repository. The working folder should
+    be part of a already git repository
+
+    """
+    try:
+        import git
+
+        repo = git.Git()
+        return repo.log("--pretty=format:'%h'", "-n 1").replace("'", "")
+    except Exception as e:
+        print(e)
+        return ""
+
+
+if ".dev" in __version__:
+    __version__ += f"+{get_commit_hash()}"

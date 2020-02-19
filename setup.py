f1762bc107832c1832ec5c35a6036bcf0560a689
Francisco Aranda <francisco@recogn.ai>
2020-02-19 16:21:58 +0100

    fixing dask dependencies (and compatible msgpack)

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ def about_info(package: str):
     """Fetch about info """
     root = os.path.abspath(os.path.dirname(__file__))
     with open(
-            os.path.join(root, "src", package.replace("-", "/"), "about.py"),
-            encoding="utf8",
+        os.path.join(root, "src", package.replace("-", "/"), "about.py"),
+        encoding="utf8",
     ) as f:
         about = {}
         exec(f.read(), about)
@@ -32,20 +32,29 @@ if __name__ == "__main__":
         packages=find_packages("src"),
         package_dir={"": "src"},
         install_requires=[
-            "dask[complete]~=2.0",
+            "dask[complete]~=2.10.0",
+            "msgpack~=0.6.0",
             "cachey~=0.1.0",  # required by dask.cache
             "pyarrow~=0.15.0",
             "ujson~=1.35",
             "pandas~=0.25.0",
             "elasticsearch<7.0",  # latest version doesn't work with dask-elk module
-            "dask-elk~=0.2.0",
+            "dask-elk~=0.3.0",
             "bokeh~=1.3",
             "xlrd~=1.2",
             "flatdict~=3.4",
             "python-dateutil<2.8.1",  # botocore (imported from allennlp) has this restriction
             "s3fs~=0.4.0",
         ],
-        extras_require={"testing": ["pytest", "pytest-cov", "pytest-pylint~=0.14.0", "black", "GitPython"]},
+        extras_require={
+            "testing": [
+                "pytest",
+                "pytest-cov",
+                "pytest-pylint~=0.14.0",
+                "black",
+                "GitPython",
+            ]
+        },
         python_requires=">=3.6.1",
         zip_safe=False,
     )
